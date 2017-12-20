2beef3ea17104e6c73adf4b8a902865db88c5574
Gil Akos <gil@astra.finance>
2017-12-20 14:11:02 -0800

    Feat: Step 15 Complete
    Updating Comment state in store

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -12,12 +12,21 @@ class Comments extends Component{
       </div>
     )
   }
+  handleSubmit(event){
+    //prevent default action of refreshing page
+    event.preventDefault()
+    //get data from form (why we specified ref on input below)
+    const { postId } = this.props.params
+    const author = this.refs.author.value
+    const comment = this.refs.comment.value
+    this.props.addComment(postId, author, comment)
+  }
   render(){
 
     return(
       <div className='comments'>
         {this.props.postComments.map(this.renderComment)}
-        <form ref='commentForm' className='comment-form'>
+        <form ref='commentForm' className='comment-form' onSubmit={this.handleSubmit}>
           <input type="text" ref="author" placeholder="author"/>
           <input type="text" ref="comment" placeholder="comment"/>
           <input type="submit" hidden />
