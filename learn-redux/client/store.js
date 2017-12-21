ddc761d55ab0f243931e8514d344c9fb210a59cf
Gil Akos <gil@astra.finance>
2017-12-21 09:42:26 -0800

    Feat: Step 18 Complete
    Hot Reloading Redux Reducers

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -16,9 +16,20 @@ const defaultState = {
 }
 
 //create the store
-const store = createStore(rootReducer, defaultState)
-//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(
+  rootReducer,
+  defaultState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export const history = syncHistoryWithStore(browserHistory, store)
+
+//add hot reloading for non-component updates
+if (module.hot) {
+  module.hot.accept('./reducers/', () => {
+    const nextRootReducer = require('./reducers/index').default
+    store.replaceReducer(nextRootReducer)
+  })
+}
 
 export default store
